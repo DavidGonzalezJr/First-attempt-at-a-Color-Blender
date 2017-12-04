3f2a7de7b92f0d18ed8e3b978c558f60157c64d3
David Gonzalez Jr <dgonzalez1@fullsail.edu>
2017-12-04 12:25:57 -0500

    Initial Commit

---
+++ GonzalezDavid_Exercise066Tests/GonzalezDavid_Exercise066Tests.swift
@@ -0,0 +1,36 @@
+//
+//  GonzalezDavid_Exercise066Tests.swift
+//  GonzalezDavid_Exercise066Tests
+//
+//  Created by David Gonzalez Jr on 12/4/17.
+//  Copyright © 2017 David Gonzalez Jr. All rights reserved.
+//
+
+import XCTest
+@testable import GonzalezDavid_Exercise066
+
+class GonzalezDavid_Exercise066Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
